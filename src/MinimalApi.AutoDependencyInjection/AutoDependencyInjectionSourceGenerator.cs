f76c899381674066fa60dbdc946f8552d17f9443
cocaine_coder <tracywang1997@outlook.com>
2025-03-15 22:35:13 +0800

    修改名称

--- src/MinimalApi.AutoDependencyInjection/AutoDependencyInjectionSourceGenerator.cs
+++ src/MinimalApi.AutoDependencyInjection/AutoDependencyInjectionSourceGenerator.cs
@@ -6,7 +6,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
-namespace MinimalApi.AutoDependencyInjection;
+namespace MinimalApi.AutoDependencyInjection.SourceGeneration;
 
 
 [Generator]
